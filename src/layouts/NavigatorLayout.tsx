1ad097eb05def07cd1b0bfa17e11744e57945338
bxstonpk <acc_gdg@outlook.com>
2026-06-06 17:00:19 +0700

    start login page

---
+++ src/layouts/NavigatorLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type NavigatorProps = {
+    /* have 3 columns 1. Logo 2. Menu 3. Profile */
+    logo: React.ReactNode
+    menu: React.ReactNode
+    profile: React.ReactNode
+}
+
+export const NavigatorLayout: React.FC<NavigatorProps> = () => {
+  return <>
+    
+  </>
+}
